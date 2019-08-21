2487805752371fabf498bc875d51c14805ec7733
SureshAlien <sureshnaga96@gmail.com>
2019-08-21 11:34:32 +0530

    added images from mprp and l

--- app/src/main/java/com/example/myapplicationfirs/Login.java
+++ app/src/main/java/com/example/myapplicationfirs/Login.java
@@ -2,12 +2,49 @@ package com.example.myapplicationfirs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
 
 public class Login extends AppCompatActivity {
+
+    Button loginButton;
+
+    JSONObject jsonObject;
+    boolean isaNewLogin;
+    String IS_NEW_LOGIN;
+
+    EditText username_entry;
+    EditText password_entry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username_entry = (EditText) findViewById(R.id.username_entry);
+        password_entry = (EditText)findViewById(R.id.password_entry);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+
+            public void onClick(View v) {
+
+                showProgress();
+                requestLogin();
+
+            }
+        });
+
+        CookieManager cookieManager = new CookieManager(new PersistentCookieStoreManager(context), CookiePolicy.ACCEPT_ORIGINAL_SERVER);
+        CookieHandler.setDefault(cookieManager);
     }
-}
+
+
+    }
